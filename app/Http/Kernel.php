c5f3c823e40f190e84fe333e6802e8e49d8c10c1
Nguyễn Việt Dũng <dung.nguyen@adamodigital.com>
2021-11-30 13:50:26 +0700

    update cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\Cors::class
         ],
 
         'api' => [
@@ -64,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check.token.success' => \App\Http\Middleware\CheckTokenSuccess::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
