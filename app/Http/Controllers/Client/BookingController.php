de5128bbe8d1c9ad5e17510984e8e5459dd55c22
Nguyễn Việt Dũng <dung.nguyen@adamodigital.com>
2021-11-30 09:37:20 +0700

    update

--- app/Http/Controllers/Client/BookingController.php
+++ app/Http/Controllers/Client/BookingController.php
@@ -27,6 +27,7 @@ class BookingController extends Controller
         $session = Session::get("order_" . $bookingReference . "");
         Session::forget("order_" . $bookingReference . "");
         $bookingOrder = $order->getAccessToken($session[0]['bookingReference']);
+        dd($session[0], $bookingOrder);
         return view('payment', ['order' => $session[0], 'bookingOrder' => $bookingOrder]);
     }
 }
