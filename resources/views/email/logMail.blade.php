aa42a67f311f62882befa3262c6c1d553d43a380
Nguyễn Việt Dũng <dung.nguyen@adamodigital.com>
2022-01-18 19:06:13 +0700

    update pretier mail

--- resources/views/email/logMail.blade.php
+++ resources/views/email/logMail.blade.php
@@ -1,1 +1,31 @@
-{!! $mail !!}
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style>
+        pre {
+            background-color: ghostwhite;
+            border: 1px solid silver;
+            padding: 10px 20px;
+            margin: 20px;
+        }
+
+        .json-key {
+            color: brown;
+        }
+
+        .json-value {
+            color: navy;
+        }
+
+        .json-string {
+            color: olive;
+        }
+    </style>
+</head>
+
+<body class="body">
+    {!! $mail !!}
+</body>
+
+</html>
