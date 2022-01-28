23a4f34ac36e5e9549c946987b59dabdd76642f0
Nguyễn Việt Dũng <dung.nguyen@adamodigital.com>
2022-01-28 10:53:53 +0700

    update

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -32,8 +32,8 @@ $header_data = curl_getinfo($curl);
 $orderDetail = json_decode($bookingOrder->CallApiDetailBooking($bookingOrder));
 if (isset($orderDetail->bookingReference)) {
     $email = @$orderDetail->products[0]->redeemers[0]->email ?: "james.nguyen@adamosoft.com";
-    // $agentEmail = @$orderDetail->agentUser->email ?: "james.nguyen@adamosoft.com";
-    $agentEmail = "james.nguyen@adamosoft.com";
+    $agentEmail = @$orderDetail->agentUser->email ?: "james.nguyen@adamosoft.com";
+    // $agentEmail = "james.nguyen@adamosoft.com";
     dispatch(new sendMail( $email, $orderDetail ));
     dispatch(new sendAgent( $agentEmail, $orderDetail ));
 }
