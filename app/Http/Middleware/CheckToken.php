8c925c58c6580da9a6747921fb4acb56891a277a
Nguyễn Việt Dũng <dung.nguyen@adamodigital.com>
2022-02-07 15:31:32 +0700

    debug

--- app/Http/Middleware/CheckToken.php
+++ app/Http/Middleware/CheckToken.php
@@ -38,7 +38,7 @@ class CheckToken
         $checkAuth = json_decode($this->quote->CallApi($url, $token), true);
 
         if (!@$checkAuth['bookingReference']) {
-            dd($request->hasHeader('FT_token'), $url, $token);
+            dd($request->header(), $url, $token);
             return response()->json([
                 'message' => __('auth.unauthenticated')
             ], 401);
