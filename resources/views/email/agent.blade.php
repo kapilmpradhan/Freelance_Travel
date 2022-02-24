15b9686514c4d5986578839e03d2c06c2afb7443
james.nguyen <james.nguyen@adamosoft.com>
2022-02-24 14:41:41 +0700

    update font

--- resources/views/email/agent.blade.php
+++ resources/views/email/agent.blade.php
@@ -3,8 +3,7 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0,user-scalable=0">
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,700;1,500&display=swap"
-        rel="stylesheet">
+
     <title>Freelancer Travel</title>
     <style>
         .body {
