fc3eb6ccf71f87e649e9d358f8ec943ee4eb6eae
Adam Tong <anh.tong.0507@gmail.com>
2023-03-01 10:24:04 +0700

    update env value

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'base_url' => env('APP_BASE_URL', 'https://app.freelance-travel.com/')
+    'base_url' => (env('APP_BASE_URL') !== null && env('APP_BASE_URL') !== '') ? env('APP_BASE_URL') : 'https://app.freelance-travel.com/'
 ];
