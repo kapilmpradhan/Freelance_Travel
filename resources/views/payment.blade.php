9c2acd51535688da829f556cb5d267ec342fe5c0
Adam Tong <anh.tong.0507@gmail.com>
2023-03-08 11:31:26 +0700

    update text in email share link page

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -36,7 +36,7 @@ if (isset($orderDetail->bookingReference)) {
     $agentEmail = @$orderDetail->agentUser->email ?: "james.nguyen@adamosoft.com";
     // $agentEmail = "james.nguyen@adamosoft.com";
     dispatch(new sendMail( $email, $orderDetail ));
-    dispatch(new sendAgent( $agentEmail, $orderDetail ));
+    dispatch(new sendAgent, $orderDetail ));
 }
 
 switch ($order['slug']) {
