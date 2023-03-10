e1c4af60d8a9e8c3db13b72dc0bb53e943120345
Adam Tong <anh.tong.0507@gmail.com>
2023-03-10 22:42:22 +0700

    update email footer

--- resources/views/email/email.blade.php
+++ resources/views/email/email.blade.php
@@ -190,13 +190,15 @@
         }
 
         .footer-freelance {
-            display: flex;
+            width: 50%;
+            margin: 0 auto;
         }
 
         .footer-freelance .logo-bottom {
             display: flex;
             height: 50px;
         }
+
         .logo img {
             max-width: 400px;
         }
@@ -215,8 +217,8 @@
 
         .footer-left,
         .footer-right {
-            width: 50%;
-            margin-top: 30px;
+            width: 65%;
+            margin-top: 20px;
         }
 
         .icon-item {
@@ -466,7 +468,8 @@
                 <div class="footer-freelance">
                     <div class="footer-left">
                         <div class="logo-bottom">
-                            <img src="https://confirmation.freelance-travel.com/images/new-logo-white.png" alt="" srcset="">
+                            <img src="https://confirmation.freelance-travel.com/images/new-logo-white.png"
+                                alt="" srcset="">
                         </div>
                     </div>
                     <div class="footer-right">
