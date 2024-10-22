a52f1a1a145db79399cc81aa013b204c9c2f965a
asheshbh11 <136300236+asheshbh11@users.noreply.github.com>
2024-10-22 13:34:33 +0545

    feat(google): check if google client is from our know google clients

--- app/Services/GoogleService.php
+++ app/Services/GoogleService.php
@@ -7,6 +7,15 @@ use Illuminate\Support\Facades\Http;
 
 class GoogleService
 {
+    public function googleTokenDetail($googleToken)
+    {
+        $response = Http::withToken($googleToken)->get('https://oauth2.googleapis.com/tokeninfo');
+        if ($response->successful()) {
+            return $response->json();
+        }
+        return false;
+    }
+
     public function googleUserDetail($accessToken)
     {
         $response = Http::withToken($accessToken)->get('https://www.googleapis.com/oauth2/v3/userinfo');
