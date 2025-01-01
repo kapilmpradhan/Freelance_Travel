620eb4a6b98fcc36123454a5f1aea8ff3af2c0b1
yonjans <95112918+yonjans@users.noreply.github.com>
2025-01-01 23:31:34 +0545

    fix(otp): no query results for model error (#95)

--- app/Http/Controllers/Api/OtpController.php
+++ app/Http/Controllers/Api/OtpController.php
@@ -61,7 +61,7 @@ class OtpController extends BaseController
         if ($is_otp_verfied['success'] == false) {
             return $this->sendError($is_otp_verfied['error']);
         }
-        $updated_user = User::findOrFail($user->id);
+        $updated_user = User::findOrFail($user->uuid);
         if ($updated_user->profile_status == 'in_progress') {
             UserProfileAgentJob::dispatch($updated_user);
         }
