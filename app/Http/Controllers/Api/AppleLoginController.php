4e07cb9282cb4352a08a65b0c20a4d44aabcb258
ashesh11 <ashesh.bh@gmail.com>
2025-11-06 13:59:23 +0545

    fix: apple email verification

--- app/Http/Controllers/Api/AppleLoginController.php
+++ app/Http/Controllers/Api/AppleLoginController.php
@@ -118,7 +118,7 @@ class AppleLoginController extends BaseController
 
         $user->verified_email = $data['new_email'];
         $user->save();
-        SendProfileEmailOtp::dispatch($user->uuid);
+        SendProfileEmailOtp::dispatch($user->uuid, app('platform'));
 
         return $this->sendResponse('OTP sent to ' . $data['new_email']);
     }
