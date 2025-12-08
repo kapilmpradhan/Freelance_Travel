6e936432eb940fbdf07929f9b74d5e22ed7a56ec
ashesh11 <ashesh.bh@gmail.com>
2025-12-08 10:50:41 +0545

    fix: cart session peterpans endpoint

--- routes/peterPansApi.php
+++ routes/peterPansApi.php
@@ -75,22 +75,20 @@ Route::group(['namespace' => 'App\Http\Controllers\Api', 'prefix' => 'cart', 'mi
     Route::post('items/v2', [CartItemController::class, 'addItemsToCartV2']);
     Route::get('items', [CartItemController::class, 'getItemsInCart']);
     Route::delete('remove/items', [CartItemController::class, 'removeItemsFromCart']);
-    Route::put('items/v2', [CartItemController::class, 'setBookingDataV2']);
+    Route::put('items', [CartItemController::class, 'setBookingData']);
     Route::get('redeemers', [RedeemerController::class, 'listRedeemers']);
     Route::post('redeemers', [RedeemerController::class, 'addRedeemer']);
     Route::delete('redeemers/{redeemerId}', [RedeemerController::class, 'removeRedeemer']);
+    Route::post('discount/v2', [CartItemController::class, 'getDiscountPercentageV2']);
 });
 
 Route::group(['namespace' => 'App\Http\Controllers\Api', 'prefix' => 'cart', 'middleware' => 'auth.jwt'], function () {
-    Route::put('items', [CartItemController::class, 'setBookingData']);
-    Route::delete('/remove/items', [CartItemController::class, 'removeItemsFromCart']);
     Route::post('order', [CartItemController::class, 'submitOrder']);
     Route::post('order/v2/direct-purchase/v2', [CartItemController::class, 'directPurchaseV2']);
     Route::get('bookings', [CartItemController::class, 'getBookings']);
     Route::delete('remove/{cartItemId}', [CartItemController::class, 'removeCartItem']);
     Route::post('items/to-quote', [CartItemController::class, 'addExistingCartItemsToQuote']);
     Route::post('items/to-quote/{quoteId}', [CartItemController::class, 'addExistingCartItemsToQuote']);
-    Route::post('discount/v2', [CartItemController::class, 'getDiscountPercentageV2']);
     Route::put('items/update', [CartItemController::class, 'updateWithLatestDetails']);
     Route::put('convert/session/{sessionId}', [CartItemController::class, 'convertSessionItemsToCartItems']);
 });
