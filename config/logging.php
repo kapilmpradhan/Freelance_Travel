672915801dd371365bbc8f8b41e2933c18760df1
Brad Lilly <brad@itbasecamp.com>
2026-01-05 16:22:18 +1100

    retack removal and add loki json take 2

--- config/logging.php
+++ config/logging.php
@@ -67,7 +67,7 @@ return [
 
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'console'],
+            'channels' => ['single', 'loki_json' ,'console'],
             'ignore_exceptions' => false,
         ],
 
